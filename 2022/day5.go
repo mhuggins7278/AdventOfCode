2e2d5eebeebc54bae0a2afbfc7bb3f5023be6f1c
Mark Huggins <mhuggins@glgroup.com>
2022-12-05 22:44:54 -0500

    works now I need to refactor

--- 2022/day5.go
+++ 2022/day5.go
@@ -1,41 +1,41 @@
 package twozerotwotwo
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
+	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
-
 	// "reflect"
-	"github.com/mhuggins7278/AdventOfCode/utils"
+	// "github.com/mhuggins7278/AdventOfCode/utils"
 )
 
 func Day5() {
-	scanner := utils.GetFileScanner("./2022/data/day4.txt")
 	input := make([]string, 0)
+	inputFile, _ := os.ReadFile("./2022/data/day5.txt")
+	input = strings.Split(string(inputFile), "\n\n")
 
-	for scanner.Scan() {
-		var row = scanner.Text()
-		input = append(input, row)
-	}
+	log.Printf("Part 1 input: %v", input)
 
-	// var part1 = Day5Part1(input)
-	// log.Printf("Part 1: %v", part1)
+	part1 := Day5Part1(input)
+	part2 := Day5Part2(input)
+	log.Printf("Part 1: %v", part1)
+	log.Printf("Part 2: %v", part2)
 	// var part2 = Day4Part2(input)
 	// log.Printf("Part 2: %v", part2)
 }
 
 func buildStacks(input string) map[string][]string {
-	space := regexp.MustCompile(`\s+`)
+	space := regexp.MustCompile(`\s{4}`)
 	stacks := make(map[string][]string)
 	rows := strings.Split(input, "\n")
 	rows = rows[:len(rows)-1]
 	for _, row := range rows {
-		log.Printf("row: %v", row)
 		row = space.ReplaceAllString(row, " ")
-    row = strings.ReplaceAll(row, "[", "")
-    row = strings.ReplaceAll(row, "]", "")
+		row = strings.ReplaceAll(row, "[", "")
+		row = strings.ReplaceAll(row, "]", "")
 		row := strings.Split(row, " ")
 		log.Printf("row: %v", row)
 		for i, _ := range row {
@@ -64,30 +64,93 @@ func reverseItemsToMove(stack []string) []string {
 
 func Day5Part1(input []string) string {
 	var nonAlphanumericRegex = regexp.MustCompile(`[^0-9]+`)
-	space := regexp.MustCompile(`\s+`)
+	// space := regexp.MustCompile(`\s+`)
 	stackString := input[0]
+	// log.Printf("stackString: %v", stackString)
 	stacks := buildStacks(stackString)
-	log.Printf("Initial stacks: %v", stacks)
+	// log.Printf("Initial stacks: %v", stacks)
 	moves := strings.Split(input[1], "\n")
+	log.Printf("moves: %v", moves)
+
 	for _, move := range moves {
-		fmt.Println(move)
-		log.Printf("stacks: %v", stacks)
-		move := strings.Split(space.ReplaceAllString(nonAlphanumericRegex.ReplaceAllString(move, ""), ""), "")
-		quantity, _ := strconv.Atoi(move[0])
+		log.Println(move)
+		// log.Printf("stacks: %v", stacks)
+		move := strings.Split(strings.Trim(nonAlphanumericRegex.ReplaceAllString(move, " "), " "), " ")
+		log.Printf("move: %v", move)
+		quantity, err := strconv.Atoi(move[0])
+		if err != nil {
+      log.Println("End of input reached")
+			break
+		}
 		from := move[1]
 		to := move[2]
 		itemsToMove := make([]string, 0)
+		// log.Printf("Move %v from: %v to: %v",quantity, from, to)
 		newSource := make([]string, 0)
 		itemsToMove = reverseItemsToMove(append(itemsToMove, stacks[from][0:quantity]...))
+		log.Printf("itemsToMove: %v", itemsToMove)
 		newSource = append(newSource, stacks[from][quantity:]...)
 		newTarget := append(itemsToMove, stacks[to]...)
 		stacks[to] = newTarget
 		stacks[from] = newSource
 	}
-	answer := make([]string, 0) 
-  for _, stack := range stacks {
-    answer = append(answer, stack[0])
-  } 
+	log.Printf("stacks: %v", stacks)
+	answer := make([]string, 0)
+	keys := make([]int, 0)
+	for key, _ := range stacks {
+		key, _ := strconv.Atoi(key)
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		answer = append(answer, stacks[strconv.Itoa(key)][0])
+	}
+	log.Printf("answer: %v", answer)
+	return strings.Join(answer, "")
+}
+func Day5Part2(input []string) string {
+	var nonAlphanumericRegex = regexp.MustCompile(`[^0-9]+`)
+	// space := regexp.MustCompile(`\s+`)
+	stackString := input[0]
+	// log.Printf("stackString: %v", stackString)
+	stacks := buildStacks(stackString)
+	// log.Printf("Initial stacks: %v", stacks)
+	moves := strings.Split(input[1], "\n")
+	log.Printf("moves: %v", moves)
+
+	for _, move := range moves {
+		log.Println(move)
+		// log.Printf("stacks: %v", stacks)
+		move := strings.Split(strings.Trim(nonAlphanumericRegex.ReplaceAllString(move, " "), " "), " ")
+		log.Printf("move: %v", move)
+		quantity, err := strconv.Atoi(move[0])
+		if err != nil {
+      log.Println("End of input reached")
+			break
+		}
+		from := move[1]
+		to := move[2]
+		itemsToMove := make([]string, 0)
+		// log.Printf("Move %v from: %v to: %v",quantity, from, to)
+		newSource := make([]string, 0)
+		itemsToMove = append(itemsToMove, stacks[from][0:quantity]...)
+		log.Printf("itemsToMove: %v", itemsToMove)
+		newSource = append(newSource, stacks[from][quantity:]...)
+		newTarget := append(itemsToMove, stacks[to]...)
+		stacks[to] = newTarget
+		stacks[from] = newSource
+	}
+	log.Printf("stacks: %v", stacks)
+	answer := make([]string, 0)
+	keys := make([]int, 0)
+	for key, _ := range stacks {
+		key, _ := strconv.Atoi(key)
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		answer = append(answer, stacks[strconv.Itoa(key)][0])
+	}
 	log.Printf("answer: %v", answer)
 	return strings.Join(answer, "")
 }
--- 2022/day5_test.go
+++ 2022/day5_test.go
@@ -2,14 +2,13 @@ package twozerotwotwo
 
 import (
 	// "fmt"
+	"log"
 	"strings"
 	"testing"
 	// "log"
 )
 
-
-var day5TestInput = 
-` [D]   
+var day5TestInput = ` [D]   
 [N] [C]    
 [Z] [M] [P]
  1   2   3 
@@ -18,17 +17,17 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2`
-      
 
 func TestDay5Part1(t *testing.T) {
 
-  sections := strings.Split(day5TestInput, "\n\n")
+	sections := strings.Split(day5TestInput, "\n\n")
 
-answer := Day5Part1(sections)
-  if answer != "CMZ" {
-    t.Fatalf(`Day 5 Part 1 answer is incorrect: %v`, answer)
+	answer := Day5Part1(sections)
+  log.Printf("Answer: %v", answer)
+	if answer != "CMZ" {
+		t.Fatalf(`Day 5 Part 1 answer is incorrect: %v`, answer)
 	} else {
-    t.Logf("Day 5 Part 1 anser is correct: %v", answer)
-  }
+		t.Logf("Day 5 Part 1 answer is correct: %v", answer)
+	}
 
 }
