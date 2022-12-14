1e39f0a0513fe0ff21ed972c1eccc3a0d66f242e
Mark Huggins <mhuggins@glgroup.com>
2022-12-14 10:34:31 -0500

    still testing

--- 2022/day10.go
+++ 2022/day10.go
@@ -9,16 +9,37 @@ import (
 	// "regexp"
 	// "github.com/k0kubun/pp"
 	// "math"
+	// "github.com/k0kubun/pp"
 	"strconv"
 	"strings"
-
-	// "github.com/k0kubun/pp"
 	// "reflect"
 	// "github.com/mhuggins7278/AdventOfCode/utils"
 	// "golang.org/x/exp/slices"
 	// "github.com/adam-lavrik/go-imath/ix"
 )
 
+type QueueItem struct {
+	tickAdded, value int
+}
+
+type Queue []QueueItem
+
+func (q *Queue) Push(n QueueItem) {
+	*q = append(*q, n)
+}
+func (q *Queue) Pop() QueueItem {
+	n := (*q)[0]
+	*q = (*q)[1:]
+	return n
+}
+func (q *Queue) Peek() QueueItem {
+	var n QueueItem
+	if len(*q) > 0 {
+		n = (*q)[0]
+	}
+	return n
+
+}
 
 func Day10() {
 	input := make([]string, 0)
@@ -26,31 +47,58 @@ func Day10() {
 	input = strings.Split(strings.TrimSuffix(string(inputFile), "\n"), "\n")
 	// part1 := Day9Part1(input, 2)
 	// log.Printf("Part 1 answer is  %v", part1)
-	part2 := Day9Part2(input, 10)
-	log.Printf("Part 2 answer is  %v", part2)
+	part1 := Day10Part1(input)
+	log.Printf("Part 2 answer is  %v", part1)
 
 }
 
 func Day10Part1(moves []string) int {
-  registerValues := make([]int, 0)
-  registerValue := 1
-  var currIncrementer int
-	for _, move := range moves {
-		// head.Move(direction, numTimes)
-		// log.Printf("Move is %v", move)
-		moveParts := strings.Split(move, " ")
-		inc, err := strconv.Atoi(moveParts[1])
-      currIncrementer = inc
-    if err != nil {
-      continue
-    } else {
-      currIncrementer = inc
-    }
-	}
-	// pp.Println(head.next)
-	retVal := len(head.next.visitedSpaces)
-	// log.Printf("Return value is %v", retVal)
+	registerValues := Queue{}
+	registerValue := 1
+	computedValues := []int{}
+	cycleToCheck := 20
+	retVal := 0
+	noops := 0
 
+	for i, move := range moves {
+		tick := i + 1
+		cycle := (tick * 2) - noops
+		log.Printf("noops is %v", noops)
+		log.Printf("Register value is %v at the start of cycle %v", registerValue, cycle)
+		moveParts := strings.Split(move, " ")
+		// log.Printf("Move: %v", moveParts)
+		if len(moveParts) == 2 {
+			inc, _ := strconv.Atoi(moveParts[1])
+			// log.Printf("Adding value: %v to queue on tick %v", inc, tick)
+			registerValues.Push(QueueItem{tick, inc})
+		} else {
+			noops = noops + 1
+      continue
+		}
+		if cycle == cycleToCheck {
+			log.Printf("Computing new value: Register value at cycle %v is %v", cycle, registerValue)
+			computedValues = append(computedValues, registerValue*cycle)
+			cycleToCheck = cycleToCheck + 40
+
+		}
+		//this has to happen at the end of the cycle nothing should be after this
+		if len(registerValues) > 0 {
+			curValue := registerValues.Peek()
+			if tick-curValue.tickAdded == 1 {
+
+				// log.Printf("Poppping %v off the queue at tick %v",curValue, tick)
+				registerValue += curValue.value
+				registerValues.Pop()
+			}
+		}
+		log.Printf("Register value is %v at the end of cycle %v", registerValue, cycle)
+
+	}
+	log.Printf("computedValues: %v", computedValues)
+
+	for _, a := range computedValues {
+		retVal = retVal + a
+	}
 	return retVal
 }
 
